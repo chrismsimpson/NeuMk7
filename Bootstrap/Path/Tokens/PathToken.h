5e62a1792351a1ba798d6e989114571231a8ef5e
Chris Simpson <chris.m.simpson@icloud.com>
2021-10-24 11:58:52 +1100

    init

--- Bootstrap/Path/Tokens/PathToken.h
+++ Bootstrap/Path/Tokens/PathToken.h
@@ -1,8 +1,26 @@
 #ifndef PATH_TOKEN_H
 #define PATH_TOKEN_H
 
-struct PathToken {
+#include "PathComponent.h"
+#include "PathPunc.h"
 
+enum PathTokenType {
+
+    pathTokenTypeComponent,
+    pathTokenTypePunc
 };
 
+union PathTokenValue {
+
+    const struct PathComponent component;
+
+    const struct PathPunc punc;
+};
+
+struct PathToken {
+
+    const enum PathTokenType tokenType;
+
+    const union PathTokenValue value;
+};
 #endif
